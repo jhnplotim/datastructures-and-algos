80a2fcaad82a4a3d687c7d4648fbf5ea73c71a9f
John Paul Otim <jhnplotim@gmail.com>
2022-08-07 11:13:13 +0200

    feat: Fibonacci Top down
    - Fibonacci Top down

---
+++ DynamicProgramming/Fibonacci/Recursive/SolutionFibTopDown.java
@@ -0,0 +1,21 @@
+package DynamicProgramming.Fibonacci.Recursive;
+
+public class SolutionFibTopDown {
+    public static void main(String[] args) {
+        System.out.println(fib(8));
+    }
+
+    private static int fib(int n) {
+        Integer[] cache = new Integer[n+1];
+        cache[0] = 0;
+        cache[1] = 1;
+        cache[2] = 1;
+
+        for(int i = 3; i < n + 1; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+
+        return cache[n];
+    }
+    
+}
