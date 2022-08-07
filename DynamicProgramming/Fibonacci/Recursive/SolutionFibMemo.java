c3406763f6c83df3fa37c1305634d580aa93ac71
John Paul Otim <jhnplotim@gmail.com>
2022-08-07 11:56:21 +0200

    feat: reordered base case for fibonacci memo
    - reordered base case for fibonacci memo

--- DynamicProgramming/Fibonacci/Recursive/SolutionFibMemo.java
+++ DynamicProgramming/Fibonacci/Recursive/SolutionFibMemo.java
@@ -20,15 +20,15 @@ public class SolutionFibMemo {
     // Find nth number in a Fibonacci sequence (memoized)
     private static Long fibMemo(int n, Long[] cache) {
         // Base case
+        if (cache[n] != null) {
+            return cache[n];
+        }
+
         if (n <= 2) {
             return 1L;
         }
 
-        if (cache[n] != null) {
-            return cache[n];
-        } else {
-            cache[n] = fibMemo(n-1, cache) + fibMemo(n-2, cache);
-            return cache[n];
-        }
+        cache[n] = fibMemo(n-1, cache) + fibMemo(n-2, cache);
+        return cache[n];
     }
 }
